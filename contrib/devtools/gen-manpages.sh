2e69a7cbb78cd90c66ba9a0a74b9396cede7198a
micha <michailduzhanski@gmail.com>
2019-11-10 16:19:47 +0200

    rename

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -4,24 +4,24 @@ TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
 SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITZECD=${BITZECD:-$SRCDIR/bitzecd}
-BITZECCLI=${BITZECCLI:-$SRCDIR/bitzec-cli}
-BITZECTX=${BITZECTX:-$SRCDIR/bitzec-tx}
+ARNAKD=${ARNAKD:-$SRCDIR/arnakd}
+ARNAKCLI=${ARNAKCLI:-$SRCDIR/arnak_cli}
+ARNAKTX=${ARNAKTX:-$SRCDIR/arnak-tx}
 
-[ ! -x $BITZECD ] && echo "$BITZECD not found or not executable." && exit 1
+[ ! -x $ARNAKD ] && echo "$ARNAKD not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-ZECVERSTR=$($BITZECCLI --version | head -n1 | awk '{ print $NF }')
+ZECVERSTR=$($ARNAKCLI --version | head -n1 | awk '{ print $NF }')
 ZECVER=$(echo $ZECVERSTR | awk -F- '{ OFS="-"; NF--; print $0; }')
 ZECCOMMIT=$(echo $ZECVERSTR | awk -F- '{ print $NF }')
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for bitzecd if --version-string is not set,
-# but has different outcomes for bitzec-cli.
+# This gets autodetected fine for arnakd if --version-string is not set,
+# but has different outcomes for arnak_cli.
 echo "[COPYRIGHT]" > footer.h2m
-$BITZECD --version | sed -n '1!p' >> footer.h2m
+$ARNAKD --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $BITZECD $BITZECCLI $BITZECTX; do
+for cmd in $ARNAKD $ARNAKCLI $ARNAKTX; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=$ZECVER --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-$ZECCOMMIT//g" ${MANDIR}/${cmdname}.1
